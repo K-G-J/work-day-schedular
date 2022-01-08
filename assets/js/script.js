88a7a2df80f32be81617a1588a0f0707ee8a6b0e
Kate Johnson <johnson.kat.g@gmail.com>
2022-01-08 17:24:48 -0500

    added timeblocks

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var currentDay = document.getElementById("currentDay")
+var timeBlockContainer = document.getElementsByClassName("container")
+var calEvent = {}
+
+// current day displayed at top of calendar 
+currentDay.textContent = moment().format('MM/DD/YYYY')
+
+// time blocks for standard business hours
+var hourBlock = document.createElement("div") 
+
+// time block color-coded to indicate past, present, or future
+
+// on time block click -> enter an event 
+
+// save button 
+
+// text and information saved to local storage 
+
+// saved events persist 
+
