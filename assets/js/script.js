9a1a9f59c95864f19fdac79d79600c22bad0eba0
Kate Johnson <johnson.kat.g@gmail.com>
2022-01-09 04:06:53 -0500

    functional local storage setting and getting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,43 @@
+// jumbotron display 
 var currentDay = document.getElementById("currentDay")
 currentDay.textContent = moment().format('MMMM Do YYYY, h:mm a')
-var timeBlockContainer = document.getElementsByClassName("container")
 
-// time block color-coded to indicate past, present, or future
-var currentTime = new Date();
-var currentHour = currentTime.getHours();
-
-var createEvent = function(calEventObj) {
-  var calEventLi = document.createElement("li")
-  calEventLi.textContent = calEventObj.eventText
-  $("#list-" + calEventObj.eventHour).append(calEventLi);
-}
+// setting and getting calendar events 
 $(".saveBtn").click(function(event) {
-  userSelection = event.target
-  var eventHour = $(userSelection).closest(".hour").html()
-  console.log(eventHour);
-  var eventText = $("input[name=event-input]").val().trim()
+  var userSelection = event.target
+  var eventHour = $(userSelection).siblings(".hour").html()
+  var eventText = $(userSelection).siblings("input[name=event-input]").val().trim()
   var calEventObj = {
     text: eventText,
     hour: eventHour
   }
-  saveCalEvent(calEventObj)
-  createEvent(calEventObj)
-});
+  saveCalEvent(calEventObj);
+    var calEventLi = document.createElement("li")
+    calEventLi.textContent = calEventObj.text
+    // add classes for styling 
+    $("#list-" + calEventObj.hour).append(calEventLi);
+  });
 function saveCalEvent(calEventObj) {
   var currentEvents = loadCalEvents();
   currentEvents.push(calEventObj);
-  localStorage.setItem("calEventObj", JSON.stringify(currentEvents))
+  localStorage.setItem("calEventObjects", JSON.stringify(currentEvents))
 }
 function loadCalEvents() {
-  var calEvents = JSON.parse(localStorage.getItem("calEventObj"));
-  if (!calEvents || !Array.isArray(calEvents)) return []
-  else return calEvents;
+  var calEventsArr = JSON.parse(localStorage.getItem("calEventObjects"));
+  if (!calEventsArr || !Array.isArray(calEventsArr)) return []
+  else return calEventsArr;
 }
-loadCalEvents();
+var showCalendar = function() {
+  var currentEvents = loadCalEvents();
+  for (var item of currentEvents) {
+    var calEventLi = document.createElement("li")
+    calEventLi.textContent = item.text
+    // add classes for styling 
+    $("#list-" + item.hour).append(calEventLi);
+  }
+}
+showCalendar();
+
+// time block color-coded to indicate past, present, or future
+var currentTime = new Date();
+var currentHour = currentTime.getHours();
