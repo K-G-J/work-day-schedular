21d109df81e2a5bb2a240d09ee712ddc4c016025
Kate Johnson <johnson.kat.g@gmail.com>
2022-01-08 20:10:40 -0500

    implemented localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,96 @@
 var currentDay = document.getElementById("currentDay")
+currentDay.textContent = moment().format('MMMM Do YYYY, h:mm a')
 var timeBlockContainer = document.getElementsByClassName("container")
-var calEvent = {}
-
-// current day displayed at top of calendar 
-currentDay.textContent = moment().format('MM/DD/YYYY')
-
-// time blocks for standard business hours
-var hourBlock = document.createElement("div") 
+var calEvent = []
 
 // time block color-coded to indicate past, present, or future
+var currentTime = new Date();
+var currentHour = currentTime.getHours();
 
-// on time block click -> enter an event 
+var getEventInfo = function() {
+    $.each(hourBlocks.hourInfo,function() {
+        //error happening here 
+        var eventTime = $(this).time
+        var eventText = $("input[name='eventInput']").val()
+       })
+    return eventTime, eventText
+}
 
-// save button 
+var saveButton = document.createElement("button")
+saveButton.textContent = "Save"
+saveButton.className = "saveBtn"
+$(".jumbotron").append(saveButton)
+saveButton.addEventListener("click", function(){
+        // get values
+        getEventInfo();
+        // save in events array
+        calEvent.push({
+            time: eventTime,
+            input: eventText
+        });
+        saveCalEvents(calEvent);
+});
 
-// text and information saved to local storage 
+var loadCalEvents = function() {
+    calEvent = JSON.parse(localStorage.getItem("calEvent"));
+    if (!calEvent) {
+        calEvent = [];
+    }
+    $.each(calEvent, function() {
+        $('.container').append('<hourBlocks class="wrapper"><h3>'+block.time+'<p>'+block.input+'</p></hourBlocks>')
+        $(this).addClass("cal-list-item")
+    });
+  };
+  
+  var saveCalEvents = function(calEvent) {
+    localStorage.setItem("calEvent", JSON.stringify(calEvent));
+  };
 
-// saved events persist 
+var hourBlocks = {
+    hourInfo: [
+      {
+        time: 9,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 10,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 11,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 12,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 1,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 2,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 3,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 4,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },
+      {
+        time: 5,
+        input: this.innerHTML = "<input type='text' name='eventInput' class='event-forum' placeholder='' autocomplete='off' /><span class='oi oi-calendar'></span>"
+      },     
+    ]
+  };
+$.each(hourBlocks.hourInfo,function(i,block){
+ $('.container').append('<hourBlocks class="wrapper"><h3>'+block.time+'<p>'+block.input+'</p></h3></hourBlocks>')
+ $(this).addClass("cal-list-item")
+})
+
+// load calendar events
+loadCalEvents();
 
